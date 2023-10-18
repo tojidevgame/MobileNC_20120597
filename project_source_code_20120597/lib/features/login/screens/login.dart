c9c98f4efcae754e57ab9342d087c8f4d40e13c1
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-10-18 23:31:55 +0700

    add image to screen login

---
+++ project_source_code_20120597/lib/features/login/screens/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+
+
+class LoginScreenState extends State<LoginScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/login/img_login.png')
+          ]
+        ),
+      ),
+    );
+  }
+}
--- project_source_code_20120597/test/widget_test.dart
+++ project_source_code_20120597/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project_source_code_20120597/main.dart';
+import 'package:lettutor_mobile_toji/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
