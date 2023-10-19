1def9dd8d2453dc614be343c06bd2c4c0c5c95ee
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-10-19 22:34:09 +0700

    description

--- project_source_code_20120597/lib/features/login/screens/login.dart
+++ project_source_code_20120597/lib/features/login/screens/login.dart
@@ -9,19 +9,35 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-
-
-class LoginScreenState extends State<LoginScreen>{
+class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/login/img_login.png')
-          ]
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                  child: Image.asset('assets/login/img_login.png')),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Text(
+                    "Say hello to your English tutors",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: Text(
+                    "Become fluent faster through one on one video chat lessons tailored to your goals.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ))
+            ]),
       ),
     );
   }
