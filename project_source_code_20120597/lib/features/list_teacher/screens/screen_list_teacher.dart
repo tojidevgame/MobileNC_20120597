96b4cb4da61d2171281f23a8fc0bc46d9b5510a9
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-10-22 10:24:35 +0700

    general info tutor

--- project_source_code_20120597/lib/features/list_teacher/screens/screen_list_teacher.dart
+++ project_source_code_20120597/lib/features/list_teacher/screens/screen_list_teacher.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lettutor_mobile_toji/commons/const_var.dart';
+import 'package:country_flags/country_flags.dart';
 
 class TutorScreen extends StatefulWidget {
   const TutorScreen({Key? key}) : super(key: key);
@@ -11,6 +12,21 @@ class TutorScreen extends StatefulWidget {
 }
 
 class TutorScreenState extends State<TutorScreen> {
+  final List<String> cardEnglishTypeData = [
+    'Tất cả',
+    'Tiếng Anh cho trẻ em',
+    'Giao tiếp',
+    'Tiếng Anh cho công việc',
+    'STARTERS',
+    'MOVERS',
+    'FLYERS',
+    'KET',
+    'PET',
+    'IELTS',
+    'TOELF',
+    'TOEIC'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +69,14 @@ class TutorScreenState extends State<TutorScreen> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    const Text('Find a tutor'),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Text(
+                        'Find a tutor',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
                     const TextField(
                         autocorrect: false,
                         enableSuggestions: false,
@@ -74,14 +97,59 @@ class TutorScreenState extends State<TutorScreen> {
                           contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
                           hintText: 'Enter tutor name...',
                         )),
-                    DropdownButtonFormField<String>(
-                      items: const [
-                        DropdownMenuItem(value: 'Cac', child: Text('Cac')),
-                        DropdownMenuItem(value: 'Hehe', child: Text('Hehe')),
-                        DropdownMenuItem(value: 'Hihi', child: Text('Hehe')),
-                        DropdownMenuItem(value: 'Haha', child: Text('Hehe')),
-                      ],
-                      onChanged: (value) {},
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+                      child: SizedBox(
+                        height: 50,
+                        child: DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            hintText: 'Chọn quốc tịch gia sư',
+                            contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: primaryColor),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12)),
+                                gapPadding: 1),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: primaryColor),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12)),
+                                gapPadding: 1),
+                          ),
+                          items: const [
+                            DropdownMenuItem(
+                                value: '1', child: Text('Gia Sư Nước Ngoài')),
+                            DropdownMenuItem(
+                                value: '2', child: Text('Gia Sư Việt Nam')),
+                            DropdownMenuItem(
+                                value: '3',
+                                child: Text('Gia Sư Tiếng Anh Bản Ngữ')),
+                          ],
+                          onChanged: (value) {},
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 15, 0, 20),
+                      child: Wrap(
+                        spacing: 5,
+                        runSpacing: 5,
+                        children: cardEnglishTypeData
+                            .map((e) => CardEnglishType(
+                                  englishType: e,
+                                  onPressed: () {},
+                                ))
+                            .toList(),
+                      ),
+                    ),
+                    const Text(
+                      'Recommended tutors',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(1),
+                      child: GeneralInfoTutor(),
                     )
                   ],
                 )),
@@ -89,5 +157,180 @@ class TutorScreenState extends State<TutorScreen> {
         ],
       ),
     ));
+  }
+}
+
+class CardEnglishType extends StatefulWidget {
+  final String englishType;
+  final VoidCallback onPressed;
+
+  const CardEnglishType(
+      {super.key, required this.englishType, required this.onPressed});
+
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: no_logic_in_create_state
+    return CardEnglishTypeState(text: englishType, onPressed: onPressed);
+  }
+}
+
+class CardEnglishTypeState extends State<CardEnglishType> {
+  final String text;
+  final VoidCallback onPressed;
+
+  CardEnglishTypeState({required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.fromLTRB(8, 1, 8, 1), // Padding for the button
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 228, 230, 235)), // Màu nền
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+            ),
+          )),
+    );
+  }
+}
+
+class GeneralInfoTutor extends StatefulWidget {
+  const GeneralInfoTutor({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: no_logic_in_create_state
+    return GeneralInfoTutorState(
+        avatar: Image.asset('assets/common/img_user.png'),
+        name: 'Not Found',
+        rate: 4,
+        cardEnglishTypeData: ['English1', 'English2', 'English3']);
+  }
+}
+
+class GeneralInfoTutorState extends State<GeneralInfoTutor> {
+  final Image avatar;
+  final String name;
+  final int rate;
+  final List<String> cardEnglishTypeData;
+
+  GeneralInfoTutorState(
+      {required this.avatar,
+      required this.name,
+      required this.rate,
+      required this.cardEnglishTypeData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(5),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 2.0,
+              )
+            ],
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(flex: 1, child: Container()),
+                      Expanded(
+                          flex: 4,
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 80,
+                                width: 80,
+                                child: avatar,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(1),
+                                child: Text(
+                                  name,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(1),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: 30,
+                                      height: 30,
+                                      child: CountryFlag.fromCountryCode('ES'),
+                                    ),
+                                    const Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                        child: Text('ES'))
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.all(1),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: List.generate(5, (index) {
+                                        return GestureDetector(
+                                          child: Icon(
+                                            Icons.star,
+                                            size: 30,
+                                            color: index < rate
+                                                ? Colors.amber
+                                                : const Color.fromARGB(
+                                                    255, 78, 78, 78),
+                                          ),
+                                        );
+                                      }))),
+                              Padding(
+                                padding: const EdgeInsets.all(1),
+                                child: Wrap(
+                                  spacing: 5,
+                                  runSpacing: 5,
+                                  children: cardEnglishTypeData
+                                      .map((e) => CardEnglishType(
+                                            englishType: e,
+                                            onPressed: () {},
+                                          ))
+                                      .toList(),
+                                ),
+                              )
+                            ],
+                          )), // Avatar
+                      Expanded(flex: 1, child: Container()), // Heart
+                    ],
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0))
+              ],
+            ),
+          ),
+        ));
   }
 }
